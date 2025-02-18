9a1d6be2bf3239b71bf2ecc2d9ffbdd7a20dcf62
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-02-18 21:43:45 +0100

    Initial commit

--- DataIMC/urls.py
+++ DataIMC/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import UserViewset, MealViewSet
-
+from django.http import JsonResponse
 from . import views
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -13,7 +13,11 @@ router = DefaultRouter()
 router.register(r'users', UserViewset) 
 router.register(r'meals', MealViewSet)
 
+def home(request):
+    return JsonResponse({"message": "Bienvenue sur l'API BMI Tracker !"})
+
 urlpatterns = [
+    path('', home, name='home'),
     path("api/", include(router.urls)),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
