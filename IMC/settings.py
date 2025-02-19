304bdd2462d2ae42c63d4e60379211190acf4add
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-02-19 14:03:56 +0100

    Initial commit

--- IMC/settings.py
+++ IMC/settings.py
@@ -45,7 +45,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        'rest_framework.permissions.IsAuthenticated',  # Ajout de la permission par défaut
+        'rest_framework.permissions.AllowAny',  # Ajout de la permission par défaut
     ),
 }
 
